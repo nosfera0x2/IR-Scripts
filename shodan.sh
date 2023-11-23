c499c94c197e0ac3a37c52d4294be96db483f4c0
nosfera0x2 <82817752+nosfera0x2@users.noreply.github.com>
2023-11-23 10:46:52 -0600

    Update shodan.sh

--- shodan.sh
+++ shodan.sh
@@ -9,37 +9,20 @@ timestamp=$(date +%Y%m%d%H%M%S)
 # Output file name
 output_file="${domain}_${timestamp}_shodan_output.txt"
 
-# Initialize an empty output file
-> "$output_file"
-
-# Function to run Shodan on an IP address
-run_shodan() {
-    local ip="$1"
-    if [[ ! " ${scanned_ips[@]} " =~ " ${ip} " ]] && ! grep -q "$ip" "$output_file"; then
-        echo "Running Shodan on $ip"
-        shodan host "$ip" >> "$output_file" 2>/dev/null
-        scanned_ips+=("$ip")  # Add the IP to the scanned list
-    fi
-}
-
 # Step 1: Get subdomains using crt.sh
 subdomains=$(curl -s "https://crt.sh/?q=%.$domain&output=json" | jq -r '.[].name_value' | grep -v "CN=" | sort -u)
 
-if [ -z "$subdomains" ]; then
-    echo "No subdomains found for $domain. Exiting."
-    exit 1
-fi
+# Print "Hosts found:" and then each host on a new line
+echo "Hosts found:"
+echo "$subdomains"
 
-# Step 2: Find IP addresses for each subdomain and run Shodan
+# Step 2: Find IP addresses for each subdomain and run shodan
+echo "Scanning all *found* IP addresses with Shodan"
 for subdomain in $subdomains; do
-    host_output=$(host "$subdomain" | grep "has address" | grep "$domain")
-
-    if [ -n "$host_output" ]; then
-        ip=$(echo "$host_output" | cut -d" " -f4)
-        run_shodan "$ip"
-    else
-        echo "No IP address found for $subdomain. Skipping."
-    fi
+    host "$subdomain" | grep "has address" | grep "$domain" | while read -r line; do
+        ip=$(echo "$line" | cut -d" " -f4)
+        shodan host "$ip" >> "$output_file"
+    done
 done
 
 echo "Shodan scan results saved to $output_file"
